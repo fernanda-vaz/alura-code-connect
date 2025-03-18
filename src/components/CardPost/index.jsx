c27730dc71aaecc63e124c31b0c054862af47730
Fernanda Vaz <flauta.fernanda@gmail.com>
2025-03-18 10:59:02 -0300

    estilizacao e paginacao

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,24 +1,28 @@
 import Image from 'next/image'
 import { Avatar } from '../Avatar'
+import styles from './card.module.css'
+import Link from 'next/link'
 
 export const CardPost = ({ post }) => {
   return (
-    <article>
-      <header>
-        <figure>
+    <article className={styles.cardContainer}>
+      <header className={styles.header}>
+        <figure className={styles.imgContainer}>
           <Image
             src={post.cover}
             width={438}
             height={133}
             alt={`Capa do post de título: ${post.title}`}
+            className={styles.img}
           />
         </figure>
       </header>
-      <section>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
+      <section className={styles.section}>
+        <h2 className={styles.title}>{post.title}</h2>
+        <p className={styles.content}>{post.body}</p>
+        <Link href={'#'} className={styles.link}>Ver detalhes</Link>
       </section>
-      <footer>
+      <footer className={styles.footer}>
         <Avatar imageSrc={post.author.avatar} name={post.author.username} />
       </footer>
     </article>
