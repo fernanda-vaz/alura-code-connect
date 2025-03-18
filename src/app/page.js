c283afadbac2e8bb548147b654eb43df92c949d1
Fernanda Vaz <fernandavazdev@gmail.com>
2025-03-18 12:04:35 -0300

    projeto finalizado

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { CardPost } from '@/components/CardPost'
 import logger from '@/logger'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 // const post = {
 //   id: 1,
@@ -33,14 +34,19 @@ async function getAllPosts(page) {
   return response.json()
 }
 
-export default async function Home() {
-  const { data: posts } = await getAllPosts(1)
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1
+  const { data: posts, prev, next } = await getAllPosts(currentPage)
 
   return (
     <main className={styles.grid}>
       {posts.map((post) => (
-        <CardPost post={post} key={post.id}/>
+        <CardPost post={post} key={post.id} />
       ))}
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   )
 }
--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -3,28 +3,31 @@ import { Avatar } from '../Avatar'
 import styles from './card.module.css'
 import Link from 'next/link'
 
-export const CardPost = ({ post }) => {
+export const CardPost = ({ post, highlight }) => {
   return (
-    <article className={styles.cardContainer}>
-      <header className={styles.header}>
-        <figure className={styles.imgContainer}>
-          <Image
-            src={post.cover}
-            width={438}
-            height={133}
-            alt={`Capa do post de título: ${post.title}`}
-            className={styles.img}
-          />
-        </figure>
-      </header>
-      <section className={styles.section}>
-        <h2 className={styles.title}>{post.title}</h2>
-        <p className={styles.content}>{post.body}</p>
-        <Link href={'#'} className={styles.link}>Ver detalhes</Link>
-      </section>
-      <footer className={styles.footer}>
-        <Avatar imageSrc={post.author.avatar} name={post.author.username} />
-      </footer>
-    </article>
+    <Link href={`/posts/${post.slug}`} className={styles.link}>
+      <article className={styles.cardContainer} style={{ width: highlight ? 993 : 486 }}>
+        <header className={styles.header}>
+          <figure style={{ height: highlight ? 300 : 133 }}>
+            <Image
+              src={post.cover}
+              fill
+              alt={`Capa do post de título: ${post.title}`}
+              className={styles.img}
+            />
+          </figure>
+        </header>
+        <section className={styles.section}>
+          <h2 className={styles.title}>{post.title}</h2>
+          <p className={styles.content}>{post.body}</p>
+          <Link href={'#'} className={styles.link}>
+            Ver detalhes
+          </Link>
+        </section>
+        <footer className={styles.footer}>
+          <Avatar imageSrc={post.author.avatar} name={post.author.username} />
+        </footer>
+      </article>
+    </Link>
   )
 }
